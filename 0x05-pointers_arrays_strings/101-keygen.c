86a62d78f02e013b9a94b6698ad97ba573ef6776
MissPhumy <missphumy@gmail.com>
2023-07-24 02:14:59 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,14 +11,14 @@ int main(void)
 	time_t t;
 
 	srand((unsigned int) time(&t));
-	while (c < 402)
+	while (c < 2772)
 	{
-		r = rand() % 98;
-		if ((c + r) > 402)
+		r = rand() % 128;
+		if ((c + r) > 2772)
 			break;
 		c = c + r;
 		printf("%c", r);
 	}
-	printf("%c\n", (402 - c));
+	printf("%c\n", (2772 - c));
 	return (0);
 }
