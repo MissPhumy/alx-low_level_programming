c403780c86979ec68a0aae60ab68129695a5c4ff
MissPhumy <missphumy@gmail.com>
2023-08-24 10:47:28 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - Prints all elements of a list_t list.
  * @h: Pointer to the head of the list.
- *
+ * 
  * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
@@ -25,5 +25,5 @@ size_t print_list(const list_t *h)
 		count++;
 	}
 
-	return (count);
+	return count;
 }
